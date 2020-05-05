87ac38a4aca39c08151c90f50a0e2e8c98ea5f78
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-05-05 14:44:07 -0700

    added table for user stocks and their associations and verified in rails console

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:sandbox_publishable_api_key],
@@ -10,7 +14,7 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
-    
+
   end
 
 end
